98397163f0bc73842bfce5f2edf3aef7557b0276
Tom-Zhang86 <wzhang10@scu.edu>
2026-07-24 14:54:52 +0800

    Delete application directory

--- application/presence/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Hardware presence adapters for the application layer."""
-
-from application.presence.serial_adapter import (
-    SerialConnectionStatus,
-    SerialPresenceAdapter,
-)
-
-__all__ = ["SerialConnectionStatus", "SerialPresenceAdapter"]
